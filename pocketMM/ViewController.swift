7178d263648158e85f56ac55a0d31e846794cd39
ctkhanhly <ctkhanhly@gmail.com>
2020-04-08 11:30:49 +0700

    Initial Commit

---
+++ pocketMM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pocketMM
+//
+//  Created by Ly Cao on 4/8/20.
+//  Copyright © 2020 NYU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ pocketMMTests/pocketMMTests.swift
@@ -0,0 +1,34 @@
+//
+//  pocketMMTests.swift
+//  pocketMMTests
+//
+//  Created by Ly Cao on 4/8/20.
+//  Copyright © 2020 NYU. All rights reserved.
+//
+
+import XCTest
+@testable import pocketMM
+
+class pocketMMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
