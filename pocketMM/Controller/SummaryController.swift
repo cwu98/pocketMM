bb259866de21b691eeb3b3c4d7f40a3ee549f033
user167006 <user167006@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-04-28 00:12:20 -0400

    updates to summary view

--- pocketMM/Controller/SummaryController.swift
+++ pocketMM/Controller/SummaryController.swift
@@ -9,12 +9,76 @@
 
 import UIKit
 import Firebase
+import Charts
 
 class SummaryController: UIViewController {
+    @IBOutlet weak var pieChart:PieChartView!
     var user : User?
+    
+    //array of transaction data of type TransactionData
+    var transactionData = [TransactionData]()
+    
+    //array to hold total spending per category... this is the data for pie chart
+    var totalSpendingByCategory = [Double]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-         title = "💰Summary"
+        
+        //group based on category into dict [category_id : [...list of TransactionData objects with this category_id ] ]
+        var groupByCategory = Dictionary(grouping: transactionData, by: {$0.category} )
+
+        for (categoryID, transaction) in groupByCategory{
+            var total = transaction.reduce(0) {  $0 + $1.amount} //sum all amount from transactions in each category
+            totalSpendingByCategory.insert(total, categoryID)
+        }
+
+        updateChardData()
+        
+        
+    }
+    func updateChardData(){
+        
+        var entries: [PieChartDataEntry] = []
+        for i in 0..<totalSpendingByCategory.count {
+            let dataEntry = PieChartDataEntry(value: totalSpendingByCategory[i], label: "Category \(i)" )
+            entries.append(dataEntry)
+        }
+        let chartDataSet = PieChartDataSet(entries: entries, label:"Categories")
+        
+        let chartData = PieChartData(dataSet: chartDataSet)
+        //assign colors??
+        //let colors = [UIColor , UIColor, ...]
+        //chartDataSet.colors = colors as! [NSUIColor]
+       
+        
+        let color1 = NSUIColor(hex: 0xC39BD3) //purple
+        let color2 = NSUIColor(hex: 0xAED6F1) //blue
+        let color3 = NSUIColor(hex: 0x76D7C4) //green
+        let color4 = NSUIColor(hex: 0xF4D03F) //yellow
+        let color5 = NSUIColor(hex: 0xF39C12) //orange
+        let color6 = NSUIColor(hex: 0xEC7063) //red
+        let color7 = NSUIColor(hex: 0xB2BABB) //gray
+        let color8 = NSUIColor(hex: 0xFADBD8)   //pink
+        
+        
+        chartDataSet.colors = [color1, color2, color3, color4, color5, color6, color7, color8]
+
+        chartDataSet.drawIconsEnabled = false
+        chartDataSet.sliceSpace = 2
+        pieChart.data = chartData
+        pieChart.chartDescription?.text = "January" //month to display
+        pieChart.drawHoleEnabled = true
+        pieChart.holeRadiusPercent = 30
+        pieChart.legend.enabled = true
+        pieChart.rotationEnabled = false
+        pieChart.entryLabelColor = .white
+        pieChart.entryLabelFont = .systemFont(ofSize: 14, weight:.light)
+        
+        let l = pieChart.legend
+        l.horizontalAlignment = .center
+        l.verticalAlignment = .bottom
+        l.xEntrySpace = 0
+        l.yEntrySpace = 8
     }
 
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -26,4 +90,5 @@ class SummaryController: UIViewController {
           print ("Error signing out: %@", signOutError)
         }
     }
+    
 }
---
+++ pocketMM/View/NSUIColorHex.swift
@@ -0,0 +1,26 @@
+//
+//  NSUIColorHex.swift
+//  pocketMM
+//
+//  Created by user167006 on 4/27/20.
+//  Copyright © 2020 NYU. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+extension NSUIColor{
+    convenience init(red: Int, green: Int, blue: Int){
+        assert(red >= 0 && red<=255, "Invalid red component")
+         assert(green >= 0 && green<=255, "Invalid green component")
+         assert(blue >= 0 && blue<=255, "Invalid blue component")
+        self.init(red: CGFloat(red)/255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue)/255.0, alpha: 1.0)
+    }
+    convenience init(hex: Int){
+        self.init(hex
+            red:(hex >> 16) & 0xFF,
+            green: (hex >> 8) & 0xFF,
+            blue:hex & 0xFF)
+        
+    }
+}
