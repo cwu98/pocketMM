858ddbbf63fbf6f358f1f1184a3908e64438ad08
ctkhanhly <ctkhanhly@gmail.com>
2020-05-02 09:57:45 +0700

    some change in storyboard

--- fireTrans/functions/index.js
+++ fireTrans/functions/index.js
@@ -6,6 +6,18 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: '### FIREBASE API KEY ###',
+  authDomain: '### FIREBASE AUTH DOMAIN ###',
+  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+});
+
+var db = firebase.firestore();
 
 exports.webhook = functions.https.onRequest((request, response) => {
 
@@ -20,7 +32,61 @@ exports.webhook = functions.https.onRequest((request, response) => {
  //    && payload["new_transactions"] >= 200){
 
  // }
+
+var user = firebase.auth().currentUser;
+
+if (user) {
+  var userRef = db.collection("users").doc(user.email);
+
+// Atomically add a new region to the "regions" array field.
+userRef.update({
+    transactions: firebase.firestore.FieldValue.arrayUnion()
+});
+} else {
+  // No user is signed in.
+}
+
+
+
  response.send(request.body);
 
 
+});
+
+// Retrieve Transactions for an Item
+// https://plaid.com/docs/#transactions
+app.get('/transactions', function(request, response, next) {
+  // Pull transactions for the Item for the last 30 days
+  var startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
+  var endDate = moment().format('YYYY-MM-DD');
+  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
+    count: 250,
+    offset: 0,
+  }, function(error, transactionsResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error
+      });
+    } else {
+      prettyPrintResponse(transactionsResponse);
+      response.json({error: null, transactions: transactionsResponse});
+    }
+  });
+});
+
+
+// Retrieve real-time Balances for each of an Item's accounts
+// https://plaid.com/docs/#balance
+app.get('/balance', function(request, response, next) {
+  client.getBalance(ACCESS_TOKEN, function(error, balanceResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error,
+      });
+    }
+    prettyPrintResponse(balanceResponse);
+    response.json({error: null, balance: balanceResponse});
+  });
 });
