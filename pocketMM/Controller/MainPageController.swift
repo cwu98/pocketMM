5f3b23595a959d4e5e5a10afda9981635da462fe
ctkhanhly <ctkhanhly@gmail.com>
2020-05-04 18:52:15 +0700

    refactor loadTransactions

--- pocketMM/Controller/MainPageController.swift
+++ pocketMM/Controller/MainPageController.swift
@@ -28,31 +28,8 @@ class MainPageController: UIViewController {
         title = "💰Home"
        print("in main page controller")
         navigationItem.hidesBackButton = true
-        let today = Date()
-        var startComponent = Calendar.current.dateComponents([.year, .month, .day], from: today)
-        startComponent.month = 1
-        startComponent.day = 1
-        let dateFormatterGet = DateFormatter()
-        dateFormatterGet.dateFormat = "yyyy-MM-dd"
-        let end = dateFormatterGet.string(from: today)
-        print(end)
         
-//        if let startDate = Calendar.current.date(bySetting: .month, value: 1 , of: today){
-         if let startDate = Calendar.current.date(from: startComponent){
-           
-            let start = dateFormatterGet.string(from: startDate)
-            print("date", start, end)
-            let plaidAPIManager : PlaidAPIManager = PlaidAPIManager()
-            if let currentUser = user {
-                PlaidAPIManager.refreshTransactions(access_token: currentUser.access_token)
-                plaidAPIManager.getTransaction(accessToken: currentUser.access_token, itemId: currentUser.item_id, startDate: start, endDate: end)
-                getTransactionFromRange(startDate: start, endDate: end)
-                
-            }
-           
-            
-        }
-        
+        loadTransactions()
         print("loading transactions")
         print("retrieved transactions")
         print(allTransactions)
